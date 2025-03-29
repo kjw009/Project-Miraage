80e0cb86c451ac2f7e0a3ffbc71500b4a2c3e197
006tracer <006tracer@gmail.com>
2025-03-29 16:10:22 +0000

    Removed Unnesscary lines of code

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-# OS
-import os.path
-
-# Datetime
-from datetime import datetime
-
-# Google Client Imports
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-
 # Grayfia Class
 from Grayfia import Grayfia
-
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly', 'https://www.googleapis.com/auth/tasks.readonly'] # Add the scopes that your app will need.
 
 if __name__ == "__main__":
     grayfia = Grayfia()
