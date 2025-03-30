64f9d6cd98bc639196ad56a130de621928e0bd3a
006tracer <006tracer@gmail.com>
2025-03-30 13:25:44 +0100

    Comments on main.py

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from Grayfia import Grayfia
 
 if __name__ == "__main__":
     grayfia = Grayfia()
-
+    # If grayfia class is initialised by credentials.json
     if grayfia.creds:
+        # Fetch events and tasks
         events = grayfia.get_events()
-        all_tasks = grayfia.get_tasks()
+        tasks = grayfia.get_tasks()
 
-        for task in all_tasks:
+        # Iterate through each task in all tasks
+        for task in tasks:
             print("---Tasks---")
             tasklist  =  task.get('task_list_title')
             task_name = task.get('title')
+            # Print the corresponding tasklist and task
             print(f"Tasklist: {tasklist}\n Task Name: {task_name}")
-            
+
+    # No credentials.json file, read the error printed on terminal
     else:
         print("Authentication Failed")
